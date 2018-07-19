7b784c1501e60d78cb20691dcf21d471aaac414a
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-07-19 18:08:54 +0000

    added create-project.sh

---
+++ rootfs/usr/local/bin/create-project.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# similar to composer --create-project
+# but does not require git vcs config in advance
+
+set -x
+
+git config --global user.name "${APP_USER}"
+git config --global user.email "${APP_EMAIL}"
+
+APP_DIR="${APP_DIR:-$(pwd)/${APP_NAME}}"
+PROJECT_GIT_URL="${PROJECT_GIT_URL}"
+PROJECT_GIT_BRANCH="${PROJECT_GIT_BRANCH:-master}"
+
+if [ -n "$PROJECT_GIT_URL" ]; then
+	# sleep random amount to avoid collision
+	sleep $(( $RANDOM % 3 +  $RANDOM % 5 ))
+	if [ ! "$(ls -A ${APP_DIR})" ]; then
+		git clone -b "$PROJECT_GIT_BRANCH" "$PROJECT_GIT_URL" "$APP_DIR"
+	fi
+	if [ -f "./composer.json" ]; then
+		composer update
+	fi
+fi
+
