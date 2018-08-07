0881fd9dd1178fecd70c84a3b5835c08cf5cf4fa
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-08-07 17:06:44 +0000

    added  apache_port env var

--- rootfs/etc/entrypoint.d/apache-setup.sh
+++ rootfs/etc/entrypoint.d/apache-setup.sh
@@ -2,9 +2,11 @@
 
 export APP_USER="${APP_USER:-cyclops}"
 export APP_GROUP="${APP_GROUP:-${APP_USER}}"
+export APACHE_PORT="${APACHE_PORT:-80}"
 
 sed -i \
     -e's/#LoadModule rewrite_module/LoadModule rewrite_module/' \
     -e 's/^User .*/User ${APP_USER}/' \
     -e 's/^Group .*/Group ${APP_USER}/' \
+    -e 's/^Listen .*/Listen ${APACHE_PORT}/' \
     /etc/apache2/httpd.conf 
