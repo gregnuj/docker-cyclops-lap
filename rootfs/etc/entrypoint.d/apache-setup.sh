195033432ffc9d498b95ece657561992b91efbe6
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-08-10 15:16:59 +0000

    fixed apache-setup.sh

--- rootfs/etc/entrypoint.d/apache-setup.sh
+++ rootfs/etc/entrypoint.d/apache-setup.sh
@@ -6,11 +6,11 @@ export APACHE_PORT="${APACHE_PORT:-80}"
 export HTDOCS_DIR="${HTDOCS_DIR:-/var/www/html}"
 
 sed -i \
-    -e's/#LoadModule rewrite_module/LoadModule rewrite_module/' \
-    -e 's/^User .*/User ${APP_USER}/' \
-    -e 's/^Group .*/Group ${APP_USER}/' \
-    -e 's/^Listen .*/Listen ${APACHE_PORT}/' \
-    -e 's/AllowOverride None/AllowOverride All/' \
+    -e "s/#LoadModule rewrite_module/LoadModule rewrite_module/" \
+    -e "s/^User .*/User ${APP_USER}/" \
+    -e "s/^Group .*/Group ${APP_USER}/" \
+    -e "s/^Listen .*/Listen ${APACHE_PORT}/" \
+    -e "s/AllowOverride None/AllowOverride All/" \
     /etc/apache2/httpd.conf 
 
 # set/fix permissions for htdocs
